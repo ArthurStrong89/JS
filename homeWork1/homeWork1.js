20d25ba73307e17ed9142203ea0427842c2b7b93
ArthurStrong89 <34716007+ArthurStrong89@users.noreply.github.com>
2018-02-01 17:48:32 +0200

    Add files via upload

---
+++ homeWork1/homeWork1.js
@@ -0,0 +1,15 @@
+//Let's do our first steps)
+var x = 25;
+var y = 7.25;
+
+var colors = ["red", "green", 'blue'];
+
+text(colors.length);
+
+let x = 9; let y = 'Hello'; let z = 23;
+
+if () { console.log('SAME TYPE'); }
+
+var x = 7;
+x = x % 3;
+//new value of x=1
